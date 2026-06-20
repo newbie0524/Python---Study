923e451620bbe0575e84fa78f09dfe0bd8c471fe
강승우 <tmx_xnn@gangseung-uui-MacBookAir.local>
2026-06-20 11:45:33 +0900

    first commit

---
+++ Chapter 2/2-1.py
@@ -0,0 +1,31 @@
+a = 123
+print(type(a))
+b = 1.2
+print(type(b))
+
+a = 3
+b = 4
+print(a + b)
+print(a * b)
+print(a / b)
+print(a ** b)
+
+a = "Life is too short"
+print(type(a))
+
+a = 'I\'m \nhungry'
+print(a)
+
+a = "Life is too short"
+print(len(a))
+print(a[3])
+print(a[0])
+# INDEX는 0부터 시작한다.
+print(a[-1])
+# -는 역으로
+
+a = "Life is too short, You need Python"
+b = a[0] + a[1] + a[2] + a[3]
+print(b)
+
+
