7ee2e2655c137ef883adf8239a2ad999a5c8f01d
newbie0524 <nalaollala.sw@gmail.com>
2026-06-20 20:49:12 +0900

    변경 내용 설명

--- Chapter 2/2-1.py
+++ Chapter 2/2-1.py
@@ -28,4 +28,3 @@ a = "Life is too short, You need Python"
 b = a[0] + a[1] + a[2] + a[3]
 print(b)
 
-
---
+++ Chapter 2/2-2.py
@@ -0,0 +1,3 @@
+a = "Life is too short, You need Python"
+print(a[0:4])
+# 0 <= a < 4 까지 뽑아낸다(4번째는 포함하지 않음)
